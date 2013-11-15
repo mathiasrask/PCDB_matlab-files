da312a32617f08e4ceb08a9273ec9161d5fe277f
Andreas Bruun Okholm <aokholm@gmail.com>
2013-11-15 10:16:17 +0100

    Analytic calculation of confidence intervals

--- Statistical Validity/Stat2/stat2montecarlo_CL.m
+++ Statistical Validity/Stat2/stat2montecarlo_CL.m
@@ -36,11 +36,18 @@ histfit(tolerenceWidths)
 
 ITvals_2 = Utilities.dimAndTolToITGrade(dimensions, tolerenceWidths);
 
+
+
+
 [ITvals, dimensions, tolerenceWidths, ITvals-ITvals_2]
+
+figure()
+
+
 end 
 %r = normrnd(10,1,10,1)
 
-%% 
+%%  Calculate 
 if (1)
 
 c_pk = 5/3; % 1.6667
@@ -49,15 +56,18 @@ target = 100; % mm  (m)
 sample_size = 10; 
 sample_sets = 20;
 
+biasCorrectionFactor_c4 = sqrt(2/(sample_size - 1)) * gamma(sample_size/2)/gamma((sample_size-1)/2)
+
+
 ITG_mean = 10;
 ITG_std = 1;
-C_a = 0.2;
+C_a = 0.1;
 
 ITGradesPD = makedist('Normal', 'mu', ITG_mean, 'sigma', ITG_std);
 
 
-runs = 40;
-h = waitbar(0,'Running montecarlo simulation')
+runs = 100;
+h = waitbar(0,'Running montecarlo simulation');
 
 probability =0.025:0.025:0.975;
 
@@ -87,8 +97,12 @@ for i= 1:runs
         sampleSetPD = makedist('Normal', 'mu', mu(j), 'sigma', sigma(j));
         
         samples = icdf(sampleSetPD, rand(sample_size,1));
-
-        sample_std = std(samples);
+        
+        sample_std = std(samples)
+        %sample_std = std(samples,1)/biasCorrectionFactor_c4;
+        
+        % bias correction
+        
         sample_mean = mean(samples);
         sample_meanshift = target - sample_mean;
         sample_PCSL = Utilities.meanshiftAndStdAndCpkToPCSL(sample_meanshift,sample_std,c_pk);
@@ -116,11 +130,27 @@ for i = 1:length(probability)
     [low(i),mid(i),upp(i),wid(i)] = Utilities.ConfidenceLimit(ITGatProperbility(:,i));
 end
 
+% probability(20) = 0.5
+
+mean = mean(sets_mean)
+
 figure ()
 hold on
 plot(low,probability,'r')
 plot(mid,probability,'b')
 plot(upp,probability,'r')
+
+x = linspace(6,14,100);
+
+xpdf = pdf(ITGradesPD, x);
+xcdf = cdf(ITGradesPD, x);
+%[xc, wsi] = Utilities.wilson(xcdf*sample_sets,sample_sets,0.05);
+[xc, wsi] = binofit(xcdf*sample_sets,sample_sets,0.05);
+
+plot(x,xc, 'b-.')
+plot(x, wsi(:,1), 'r-.')
+plot(x, wsi(:,2), 'r-.')
+
 
 figure()
 plot(probability, wid)
@@ -128,8 +158,29 @@ plot(probability, wid)
 
 end
 
-
-
 %%%
 
+%%
+if (0)
+
+samples_sets = 20;
+mu = 10;
+sigma = 1;
+pd = makedist('Normal', 'mu', mu, 'sigma', sigma);
+x = linspace(6,14,100);
+
+xpdf = pdf(pd, x);
+xcdf = cdf(pd, x);
+[xc, wsi] = Utilities.wilson(xcdf*samples_sets,samples_sets,0.05);
+[xc, wsi] = binofit(xcdf*samples_sets,samples_sets,0.05);
+
+figure()
+hold on
+plot(x,xc, 'r')
+plot(x, wsi(:,1))
+plot(x, wsi(:,2))
+
+
+
+end
 
