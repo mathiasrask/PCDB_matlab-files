ed34d99b96b4d20d3059ddb52faf28aea9b26662
Mathias Møller <mathiasrask@gmail.com>
2013-11-19 16:44:19 +0100

    Data gen for presentation

---
+++ DatafromTI/Analysis.m
@@ -0,0 +1,19 @@
+clc
+clear all
+close all
+
+% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Times New Roman')
+set(0,'DefaultAxesFontSize', 9)
+
+% Change default text fonts.
+set(0,'DefaultTextFontname', 'Times New Roman')
+set(0,'DefaultTextFontSize', 9)
+
+addpath('lib/export_fig');
+
+load 'DATA.mat'
+
+
+
+
