90761e98235fcffc587363142dc40ae924761643
Andreas Bruun Okholm <aokholm@gmail.com>
2013-11-28 11:44:48 +0100

    Awesome Totally unbiased standard deviation!

--- Statistical Validity/Stat2/unbiasedStdEstimator.m
+++ Statistical Validity/Stat2/unbiasedStdEstimator.m
@@ -4,13 +4,41 @@ clear
 
 % test of unbiased estimator of standard deviation
 
-mu = 0
-sigma = 1
+mu = 0;
+sigma = 1;
+sampleSizeList = [3, 4, 5, 10];
+sampleSizeListLength = length(sampleSizeList);
+N = 10000;
 
-
+std_samplesize = zeros(sampleSizeListLength, 1);
+mean_samplesize = zeros(sampleSizeListLength, 1);
 
 tic
-for n=1:100000
-    norminv(rand(),mu, sigma);
+for j = 1:sampleSizeListLength
+    sampleSize = sampleSizeList(j);
+    
+    samples = zeros(sampleSize,1);
+    
+    biasCorrectionFactor_c4 = sqrt(2/(sampleSize - 1)) * gamma(sampleSize/2)/gamma((sampleSize-1)/2);
+    
+    std_star_size = zeros(N,1);
+    mean_star_size = zeros(N,1);
+    
+    for n=1:N
+        for i=1:sampleSize
+            samples(i) = norminv(rand(),mu, sigma);
+        end
+        std_star_size(n) = std(samples)/biasCorrectionFactor_c4;
+        mean_star_size(n) = mean(samples);
+    end
+    std_samplesize(j) = mean(std_star_size);
+    mean_samplesize(j) = mean(mean_star_size);
+    
 end
 toc
+
+
+std_samplesize
+mean_samplesize
+
+
