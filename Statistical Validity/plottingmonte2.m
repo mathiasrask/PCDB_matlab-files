74b1f1eca37c1f6c52843cdd4f535372c1226549
Andreas Bruun Okholm <aokholm@gmail.com>
2013-12-06 11:20:06 +0100

    Updated and made new plots

--- Statistical Validity/plottingmonte2.m
+++ Statistical Validity/plottingmonte2.m
@@ -13,20 +13,50 @@ set(0,'DefaultTextFontSize', 9)
 
 addpath('lib/export_fig');
 
+% IT grade
+for run=1:0
+    load 'Stat2/Size3_sample10.mat'
 
-load 'Stat2/Size3_sample10.mat'
+    f2=figure()
+    hold on
+    plot(x,xc, 'K-.')
+    plot(mid,probability,'b')
+    plot(low,probability,'r')
+    plot(upp,probability,'r')
 
-f2=figure()
-hold on
-plot(x,xc, 'K-.')
-plot(mid,probability,'b')
-plot(low,probability,'r')
-plot(upp,probability,'r')
+    legend('Actual', 'Estimated','Confidence','location','northwest')
+    xlabel('IT grade')
+    ylabel('Probability')
+    set(gcf, 'Color', 'w');
+    set(f2, 'units', 'centimeters', 'pos', [0 0 8 8])
+end
 
-legend('Actual', 'Estimated','Confidence','location','northwest')
-xlabel('IT grade')
-ylabel('Probability')
-set(gcf, 'Color', 'w');
-set(f2, 'units', 'centimeters', 'pos', [0 0 8 8])
 
-export_fig('CLW90_lines.pdf')
+% Standard deviation - confidence interval
+for i=1:1
+    load('Stat2/CLW90_std.mat')
+    
+    CLW90_std_sym = CLW90_norm1./2;
+    
+%     x = linspace(min(ITG_std), max(ITG_std), 100);
+%     
+%     p = polyfit(ITG_std,CLW90_std_sym,2)
+%     
+%     y = p(1)*x.^2 + p(2)*x + p(3); 
+
+    f= fit(ITG_std',CLW90_std_sym','exp2')
+    
+    f2=figure()
+    hold on
+    plot(f,ITG_std,CLW90_std_sym)
+
+    %legend('Actual', 'Estimated','Confidence','location','northwest')
+    xlabel('Standard deviation of IT Grade input distribution')
+    ylabel('Normalized symetric confidence interval')
+    set(gcf, 'Color', 'w');
+    set(f2, 'units', 'centimeters', 'pos', [0 0 8 8])
+    hLeg = legend('example')
+    set(hLeg,'visible','off')
+    
+    export_fig('CLW90_std.pdf')
+end
